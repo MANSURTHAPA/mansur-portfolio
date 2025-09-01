78a40fabe38c35ec378befbeb62c057efcbc4397
Thapa_Mansur_21516012_ISErepo <21516012@student.curtin.edu.au>
2025-09-01 21:10:16 +0930

    update email service

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -67,8 +67,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        "service_r2i0by4",
-        "template_mf5x3bh",
+        "service_xooz93r",
+        "template_3we0ksb",
         {
           from_name: form.name,
           to_name: "Mansur Thapa",
@@ -76,7 +76,7 @@ const Contact = () => {
           to_email: "mansurk184@gmail.com",
           message: form.message,
         },
-        "p-gXzzyvEhPaJ0XA-"
+        "eAVRn-MJsPm0HbB0Q"
       )
       .then(
         () => {
